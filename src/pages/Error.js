05843182813134471aa96c0348041eef216280b3
Rabican-code <Rabikantsingh24@gmail.com>
2022-07-01 18:14:30 +0530

    First commit

---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <div>
+      <h2> Page not found </h2>
+      <Link to='/' className="btn">
+        Home page
+      </Link>
+    </div>
+  )
+}
+
+export default Error
